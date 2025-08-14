74e70d43b79ce3d4b6b96205a33b322f2cd485fd
tarcisions <tarcisions@gmail.com>
2025-08-13 22:29:04 -0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 set -e
 
-# Migrações
 echo "Aplicando migrações..."
 python manage.py migrate --noinput
 
-# Coleta static files
 echo "Coletando arquivos estáticos..."
 python manage.py collectstatic --noinput
 
-# Inicia Gunicorn
 echo "Iniciando Gunicorn..."
 exec gunicorn robot_monitor.wsgi:application \
     --bind 0.0.0.0:8000 \
