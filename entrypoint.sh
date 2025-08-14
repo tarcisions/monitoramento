42ccadb5aee9a04b278655f73fb063be6e5f0db0
tarcisions <tarcisions@gmail.com>
2025-08-13 21:53:54 -0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 set -e
 
-# Função para aguardar serviço
-wait_for_service() {
-  local host=$1
-  local port=$2
-  echo "Aguardando $host:$port..."
-  while ! nc -z "$host" "$port"; do
-    sleep 1
-  done
-  echo "$host:$port pronto!"
-}
-
-# Espera pelos serviços
-wait_for_service "$POSTGRES_HOST" 5432
-wait_for_service "$REDIS_HOST" 6379
-
 # Migrações
 echo "Aplicando migrações..."
 python manage.py migrate --noinput
